ebe953ab658ddeab026f6fbcbfb8ad65fc1afba3
Шляпкин Максим <WhiteMaksGitLab@yandex.ru>
2020-11-16 22:04:37 +0300

    add about info in profile

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import './app.module.css';
 import React from "react";
 import style from "./app.module.css"
 import Header from "./common/Header";
+import Profile from "./components/Profile";
 
 function App() {
     return(
         <div className={style.wrapper}>
             <Header />
             <div className={style.content}>
-                <div className={style.profile}>
-                    <div className={style.prof_about}>
-                        <div className={style.prof_about_img}></div>
-                        <div className={style.prof_about_info}></div>
-                    </div>
-                </div>
+                <Profile />
             </div>
         </div>
   );
